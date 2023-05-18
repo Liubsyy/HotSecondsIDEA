034d3fc5495899a5e17ea58bccfc934719fdd198
Thanple <805685524@qq.com>
2023-05-18 18:13:36 +0800

    Update HotDeployBean.java

--- src/com/liubs/ff/hotdeploy/HotDeployBean.java
+++ src/com/liubs/ff/hotdeploy/HotDeployBean.java
@@ -4,19 +4,4 @@ import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
 
 public class HotDeployBean {
-    private HotDeployClient hotDeployClient;
-
-    public HotDeployBean() {
-    }
-    public static HotDeployBean getInstance(@NotNull Project project) {
-        return project.getService(HotDeployBean.class);
-    }
-
-    public HotDeployClient getHotDeployClient() {
-        return hotDeployClient;
-    }
-
-    public void setHotDeployClient(HotDeployClient hotDeployClient) {
-        this.hotDeployClient = hotDeployClient;
-    }
 }
